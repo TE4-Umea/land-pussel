47cd9ee467b9d04f14157591c722e8a5518811c3
timnilsson04 <timnils04@gmail.com>
2024-01-16 10:32:38 +0100

    Add home component and update app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: 'game', component: GameComponent }
+    { path: 'game', component: GameComponent },
+    { path: '', component: HomeComponent }
 ];
