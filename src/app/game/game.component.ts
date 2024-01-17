7547246413f288d50182b912599d80ae290f177a
timnilsson04 <timnils04@gmail.com>
2024-01-17 14:54:42 +0100

    Add countries.json and update game component

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterLink, RouterOutlet } from '@angular/router'
+import { } from '@storybook/angular'
 
 @Component({
     selector: 'app-game',
@@ -10,5 +11,11 @@ import { RouterLink, RouterOutlet } from '@angular/router'
     styleUrl: './game.component.css'
 })
 export class GameComponent {
-    tiles = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    tiles = numberOfTiles
+    trueOrFalse = true
 }
+
+const numberOfTiles = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
