20805386a888b98af54fa6ac9db34ded924a9e1d
Albert <albert.sparrman@gmail.com>
2024-01-17 14:22:33 +0100

    Ny episk tile

---
+++ src/app/tile/tile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tile',
+  standalone: true,
+  imports: [],
+  templateUrl: './tile.component.html',
+  styleUrl: './tile.component.css'
+})
+export class TileComponent {
+
+}
--- src/stories/tile.stories.ts
+++ src/stories/tile.stories.ts
@@ -1,32 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 
-import { CheckboxComponent } from './tile.component';
+import { TileComponent } from '../app/tile/tile.component';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-const meta: Meta<CheckboxComponent> = {
+const meta: Meta<TileComponent> = {
   title: 'Components/Tile',
-  component: CheckboxComponent,
+  component: TileComponent,
   tags: ['autodocs'],
-  render: (args: CheckboxComponent) => ({
+  render: (args: TileComponent) => ({
     props: {
-      backgroundColor: null,
       ...args,
     },
   }),
-  argTypes: {
-    backgroundColor: {
-      control: 'color',
-    },
-  },
 };
 
 export default meta;
-type Story = StoryObj<CheckboxComponent>;
+type Story = StoryObj<TileComponent>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
   },
 };
