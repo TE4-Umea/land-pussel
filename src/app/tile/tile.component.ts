cc77328a45cf91fe000e421e4e018492f9b5f5d0
Albert <albert.sparrman@gmail.com>
2024-01-18 13:46:48 +0100

    episka nya tiles 2

--- src/app/tile/tile.component.ts
+++ src/app/tile/tile.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-tile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tile.component.html',
   styleUrl: './tile.component.css'
 })
 export class TileComponent {
-
+  
 }
