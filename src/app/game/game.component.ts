df027a55d276d0ddd4ef29986bd553f36ef515e5
timnilsson04 <timnils04@gmail.com>
2024-01-18 12:26:37 +0100

    Update game component and add countries.json

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,24 +3,23 @@ import { CommonModule } from '@angular/common'
 import { RouterLink, RouterOutlet } from '@angular/router'
 import { } from '@storybook/angular'
 import { ReactiveFormsModule } from '@angular/forms'
-import { ElementRef } from '@angular/core';
-
-let tileCount = 9
-
+import { ElementRef } from '@angular/core'
+import countries from '../../assets/countries.json'
 @Component({
-  selector: 'app-game',
-  standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, ReactiveFormsModule],
-  templateUrl: './game.component.html',
-  styleUrl: './game.component.css'
+    selector: 'app-game',
+    standalone: true,
+    imports: [CommonModule, RouterLink, RouterOutlet, ReactiveFormsModule],
+    templateUrl: './game.component.html',
+    styleUrl: './game.component.css'
 })
 
 export class GameComponent {
-  trueOrFalse = true
-  @ViewChild('form') myElement!: ElementRef;
-  ngAfterViewInit(): void {
-    console.log(this.myElement.nativeElement)
-  }
+    countries = countries
+    checkboxCheckBoolean = true
+    tiles = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @ViewChild('form') myElement!: ElementRef
+    ngAfterViewInit(): void {
+        console.log(this.myElement.nativeElement)
+    }
 
-  tiles = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 }
