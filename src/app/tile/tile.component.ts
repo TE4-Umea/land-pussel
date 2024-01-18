b32b7899e8b0b1a090bd3a82a40a5cb7ed448c03
RasmusJohansson04 <ras.johansson04@gmail.com>
2024-01-18 13:49:27 +0100

    Fixed the code. :thumbsup:

--- src/app/tile/tile.component.ts
+++ src/app/tile/tile.component.ts
@@ -2,35 +2,31 @@ import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElementRef } from '@angular/core'
 
-let id
-
 @Component({
-  selector: 'app-tile',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './tile.component.html',
-  styleUrl: './tile.component.css'
+    selector: 'app-tile',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './tile.component.html',
+    styleUrl: './tile.component.css'
 })
 export class TileComponent {
-  tileElements: Element[] = []
-  tiles = new Array(9)
-  trueOrFalse = true
+    tileElements: Element[] = []
+    tiles = new Array(9)
+    trueOrFalse = true
 
-  japp = 'japp'
+    @ViewChild('form') form!: ElementRef
 
-  @ViewChild('form') form!: ElementRef
-
-  ngAfterViewInit(): void {
-    this.tileElements = Array.from(this.form.nativeElement.children)
-    this.tileElements.forEach((tile, index) => {
-      tile.id = index.toString()
-      console.log(tile.children)
-      Array.from(tile.children).forEach((element: Element) => {
-        const labelElement = element as HTMLLabelElement;
-        labelElement.id = index.toString();
-        labelElement.htmlFor = index.toString();
-      });
-      console.log(tile.id)
-    })
-  }
+    ngAfterViewInit(): void {
+        this.tileElements = Array.from(this.form.nativeElement.children)
+        this.tileElements.forEach((tile, index) => {
+            tile.id = index.toString()
+            console.log(tile.children)
+            Array.from(tile.children).forEach((element: Element) => {
+                const labelElement = element as HTMLLabelElement;
+                labelElement.id = index.toString();
+                labelElement.htmlFor = index.toString();
+            })
+            console.log(tile.id)
+        })
+    }
 }
