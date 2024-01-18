4de5559de50d62a83e26067aa16d553581496b91
RasmusJohansson04 <ras.johansson04@gmail.com>
2024-01-18 13:37:20 +0100

    Commit and push

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,9 +1,8 @@
-import { Component, ViewChild } from '@angular/core'
+import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterLink, RouterOutlet } from '@angular/router'
 import { } from '@storybook/angular'
 import { ReactiveFormsModule } from '@angular/forms'
-import { ElementRef } from '@angular/core'
 import { TileComponent } from '../tile/tile.component'
 
 @Component({
@@ -15,20 +14,4 @@ import { TileComponent } from '../tile/tile.component'
 })
 
 export class GameComponent {
-  tileElements: Element[] = []
-  tiles = new Array(9)
-  trueOrFalse = true
-
-  @ViewChild('form') form!: ElementRef
-
-  ngAfterViewInit(): void {
-    this.tileElements = Array.from(this.form.nativeElement.children)
-    let index = 0
-    this.tileElements.forEach(tile => {
-      tile.id = index.toString()
-      console.log(tile.children)
-      index++
-      console.log(tile.id)
-    })
-  }
 }
--- src/app/tile/tile.component.ts
+++ src/app/tile/tile.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ElementRef } from '@angular/core'
 
 let id
 
 @Component({
   selector: 'app-tile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tile.component.html',
   styleUrl: './tile.component.css'
 })
 export class TileComponent {
+  tileElements: Element[] = []
+  tiles = new Array(9)
+  trueOrFalse = true
 
+  japp = 'japp'
+
+  @ViewChild('form') form!: ElementRef
+
+  ngAfterViewInit(): void {
+    this.tileElements = Array.from(this.form.nativeElement.children)
+    this.tileElements.forEach((tile, index) => {
+      tile.id = index.toString()
+      console.log(tile.children)
+      Array.from(tile.children).forEach((element: HTMLLabelElement) => {
+        element.id = index.toString()
+        element[htmlFor] = index.toString()
+      })
+      console.log(tile.id)
+    })
+  }
 }
